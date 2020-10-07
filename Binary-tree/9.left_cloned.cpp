2ec3b9339c55fce96af925731f2860d1fa50e2f6
nitish81299 <nitishkumar81299@gmail.com>
2020-10-07 22:26:59 +0530

    Transform a tree to left cloned(copy all node's data to their left)

--- Binary-tree/9.left_cloned.cpp
+++ Binary-tree/9.left_cloned.cpp
@@ -1,87 +1,73 @@
-/*
-1) Make two variables to store the data of left and right chiild.
-2) Check if the left and right child have not null values and store their values into variables.
-3) Check if their sum is equal to the root node data while recursively calling the isSum function in the 
-of statement for the further left and right child.
-4) In main function pass the root node to the isSum function.
-
-TC = O(n)
-
-*/
-
-
 #include <iostream>
 
 using namespace std;
 
 struct Node
 {
-	int key;
+	int data;
 	Node *left;
 	Node *right;
 };
 
-int isSum(Node *node)
-{
-	int left_data = 0, right_data = 0;
-
-	if (node == NULL || (node->left == NULL && node->right == NULL))
-	{
-		return 1;
-	}
-
-	else
-	{
-		if (node->left != NULL)
-			left_data = node->left->key;
-
-		if (node->right != NULL)
-			right_data = node->right->key;
-
-		if ((node->key == left_data + right_data) && isSum(node->left) && isSum(node->right))
-			return 1;
-		else
-			return 0;
-	}
-}
-
-Node* createNode(int key)
+Node *createNode(int data)
 {
 	Node *newnode = new Node();
-	newnode->key = key;
+	newnode->data = data;
 	newnode->left = NULL;
 	newnode->right = NULL;
 
 	return newnode;
 }
 
+
+Node *left_cloned(Node *root)
+{	
+	if (root == NULL)
+	{
+		return NULL;
+	}
+
+	// creates the left clones for the left and and right childerens of the root
+	Node *lcr = left_cloned(root->left);
+	Node *rcr = left_cloned(root->right);
+
+	// create a new node 
+	// Since it is left cloned, the new node is holds the same data as of root
+	// and attach the lcr of to the left of this new node and null to the its right
+	Node *newNOde = createNode(root->data);
+	newNOde->left = lcr;
+	newNOde->right = NULL;
+
+	// Fina;ly attach this new node to the left of the root
+	root->left = newNOde;
+	root->right = rcr;
+
+	return root;
+}
+
 void printtree(Node *temp)
 {
 	if (temp != NULL)
 	{
-		printtree(temp->left);
-		cout<<temp->key<<" ";
-		printtree(temp->right);
-	}	
+	printtree(temp->left);
+	cout<<temp->data<<" ";
+	printtree(temp->right);
+	}
 }
 
 int main()
-{	
-	Node *root = createNode(10);
-	root->left = createNode(8);
-	root->right = createNode(2);
-	root->left->left = createNode(3);
-	root->left->right = createNode(5);
-	root->right->right = createNode(2);
-
+{
+	Node *root = createNode(50);
+	root->left = createNode(25);
+	root->right = createNode(75);
+	root->left->left = createNode(12);
+	root->left->right = createNode(37);
+	root->left->right->left = createNode(98);
+	root->right->left = createNode(62);
+	root->right->right = createNode(18); 
+   	
+	left_cloned(root);
 	printtree(root);
-	cout<<'\n';
-
-	if (isSum(root))
-		cout<<"Satisfies";
-
-	else
-		cout<<"Unsatisfies";
 
 	return 0;
 }
