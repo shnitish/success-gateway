ad133c433b52443484c4a39c42684e4c06082485
nitish81299 <nitishkumar81299@gmail.com>
2020-10-14 20:37:52 +0530

    Update in the code

--- dynamic programming/4.coin_change_min.cpp
+++ dynamic programming/4.coin_change_min.cpp
@@ -8,9 +8,9 @@ int coinChange(int val, int coin[], int n)
 	int result[n + 1][val + 1];
 
 	// To fill the 0th row with infinity
-	for(int i = 0; i <= val; i++)
+	for(int i = 1; i <= val; i++)
 	{
-		result[0][i] = INT_MAX;
+		result[0][i] = INT_MAX - 1;
 	}
 
 	// To fill the 0th column with zero
@@ -24,13 +24,13 @@ int coinChange(int val, int coin[], int n)
 	{
 		for (int j = 1; j <= val; ++j)
 		{	
-			// IF coin value is greater than j then copy the previous value from same coloumn
-			if(coin[i - 1] > j)
-				result[i][j] = result[i - 1][j];
-
-			// if it is not then find min of (previous value of same coloumn, value at same row - cur coin value + 1)
-			else
+			// // if coin value is less than j then find min of (previous value of same coloumn, value at same row - cur coin value + 1)
+			if(coin[i - 1] <= j)
 				result[i][j] = min(result[i - 1][j] , result[i][j - coin[i - 1]] + 1);
+				
+			// IF not, then copy the previous value from same coloumn
+			else
+				result[i][j] = result[i - 1][j];
 		}
 	}
 
@@ -39,9 +39,9 @@ int coinChange(int val, int coin[], int n)
 
 int main()
 {	
-	int coin[] = {1, 5, 7, 8};
+	int coin[] = {9, 6, 5, 1};
 
-	int val = 12;
+	int val = 11;
 	int n =4;
 	cout<<coinChange(val, coin, n);
 	return 0;
