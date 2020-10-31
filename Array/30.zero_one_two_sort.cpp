a6828928f11fedfc79bc4cb7c6a69125d41944ce
nitish81299 <nitishkumar81299@gmail.com>
2020-10-31 13:21:37 +0530

    Sort an array having 0,1,2 using paritioning.

--- Array/30.zero_one_two_sort.cpp
+++ Array/30.zero_one_two_sort.cpp
@@ -1,52 +1,56 @@
 /*
-Keep three indices low = 1, mid = 1 and high = N and there are four ranges, 1 to low (the range containing 0), low to mid (the range containing 1), mid to high (the range containing unknown elements) and high to N (the range containing 2).
-Traverse the array from start to end and mid is less than high. (Loop counter is i)
-If the element is 0 then swap the element with the element at index low and update low = low + 1 and mid = mid + 1
-If the element is 1 then update mid = mid + 1
-If the element is 2 then swap the element with the element at index high and update high = high – 1 and update i = i – 1. As the swapped element is not processed
-Print the output array.
+Sort an array having only 0, 1, 2
+
+Logic: Patitioning same as quick sort
+Dutch National Flag algo
+
+TC : O(n)
+SC : O(1)
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void sortarray(int arr[], int size)
+void sortPartition(vector <int> arr)
 {
-	int low = 0;
-	int mid = 0;
-	int high = size - 1;
+    int i = 0;  // 0 to j - 1 -> 0's area 
+    int j = 0;  // j to i - 1 -> 1's area
+    int k = arr.size() - 1; // k + 1 to end -> 2's area
 
-	while(mid <= high)
-	{
-		switch(arr[mid])
-		{
-			case 0:
-				swap(arr[low], arr[mid]);
-				low++;
-				mid++;
-				break;
+    // i to k is unknown 
+    // when i crosses k the array is sorted
 
-			case 1:
-				mid++;
-				break;
+    while (i <= k)
+    {
+        if(arr[i] == 1)
+        {
+            i++;
+        }
+        
+       else if(arr[i] == 0)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+            j++;
+        }
 
-			case 2:
-				swap(arr[mid], arr[high]);
-				high--;
-				break;
-		}
-	}
+        else
+        {
+            swap(arr[i], arr[k]);
+            k--;
+        }
+    }
+
+     for(auto x: arr)
+        cout<<x<<" ";
+    
 }
 
 int main()
-{
-	int arr[] = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
-	int size = sizeof(arr)/sizeof(arr[0]);
-	sortarray(arr, size);
-	for (int i = 0; i < size; ++i)
-	{
-		cout<<arr[i]<<" ";
-	}
-	return 0;
+{   
+    vector <int> arr = {1, 1, 2, 2, 0, 1, 2, 2, 1, 0, 1, 2, 0, 2, 1};
+    sortPartition(arr);
+    return 0;
 }
